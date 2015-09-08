9bd208fde757bc18780860d6965698a90f0b13c2
Alvaro Padierna <apadierna@gmail.com>
2015-09-08 22:33:27 +0200

    apply RegExp filtering just to the 'in-project' files path
    the normalization to aboslute paths is done after the modified files
    RexExp filtering, by doing so, I avoid to miss-match all the project
    files when the RegExp pattern may be found in the project folder path
    
    Example:
    If the project is placed in `~/.git-scripts/`
    the command `git add-regex script` will match any modified file within
    the project.
    With this modification only the part of the file path under the project
    root folder will be used for filtering

--- gitutils.py
+++ gitutils.py
@@ -14,8 +14,7 @@ def get_repo_changes(ignore_untracked=False):
     """
     Return a list of modified files within a git repository
     """
-    project_root = subprocess.check_output(['git', 'root']).strip()
     project_status = subprocess.check_output(['git', 'status', '--porcelain']).rstrip().split('\n')
     if ignore_untracked:
         project_status = filter(lambda x: not x.startswith('?'), project_status)
-    return [os.path.join(project_root, i[3:]) for i in project_status]
+    return [i[3:] for i in project_status]
